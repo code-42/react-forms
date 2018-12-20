aef15048454dae44904ecd936296ce88fccdd74a
Edward Dupre <edwd42@gmail.com>
2018-12-20 12:17:05 -0500

    add form fields and onSubmit() handler

--- src/App.js
+++ src/App.js
@@ -1,12 +1,27 @@
+// source https://github.com/benawad/basic-react-form/blob/master/src/App.js
+// youtube https://www.youtube.com/watch?v=qH4pJISKeoI
+
 import React, { Component } from 'react';
 import './App.css';
 import Form from "./Form";
 
 class App extends Component {
+  state = {
+    fields: {}
+  };
+
+  onSubmit = fields => {
+    this.setState({fields});
+    console.log("App component got: ", fields);
+  };
+
   render() {
     return (
       <div className="App">
-        <Form />
+        <Form onSubmit={fields => this.onSubmit(fields)} />
+        <p>
+          {JSON.stringify(this.state.fields, null, 2)}
+        </p>
       </div>
     );
   }
